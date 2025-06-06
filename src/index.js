eb536abfc2a73aae7387cf22582125e8bffadfc3
yoon5450 <dbseodnd356@naver.com>
2025-06-07 02:41:31 +0900

    solve function refactoring

--- src/index.js
+++ src/index.js
@@ -1,40 +1,72 @@
+// import './work1.js';
+import "./work2.js";
 import { solution as solution1 } from "./work1.js";
-import { solution as solution2 } from "./work2.js";
+// import { solution as solution2 } from './work2.js';
 
-const sols = [solution1, solution2];
+const sols = [solution1];
 let pageIndex = 0;
+
+// 문제 데이터 : 일단 하드코딩해서 테스트 케이스 채우기.
+// 가능한 방법들 : JSON 데이터로 바꿔서 import하기, 백엔드 서버에 올려서 ajax로 문제 데이터 수령
+const problemExplain = `
+문제 : 가계부 정리
+
+✔️ 문제 설명
+
+당신은 가계부를 정리하려고 합니다. 가계부에는 금액과 플래그(a, s)가 번갈아가며 기록되어 있습니다.
+
+- 초기 자산은 0입니다.
+- 금액 다음에는 반드시 하나의 플래그가 옵니다.  ex) 23000a
+- 플래그 a는 수입을 의미합니다.
+- 플래그 s는 지출을 의미합니다.
+- 이러한 금액, 플래그 쌍이 문자열 전체에서 반복됩니다.
+
+주어진 가계부 기록을 보고 총 자산을 계산하는 함수를 작성하세요!
+`;
+const input = [["10000a4000s3000a"], ["5000a1000s25000a"], ["300s200a"]];
+const expectiveValue = [9000, 29000, -100];
+
+let problemSett = [[input, expectiveValue]];
 
 // dom tag 선언
 const codeEditor = document.getElementById("input-code");
 const lines = document.getElementById("line-number");
 const submitBtn = document.getElementById("submitBtn");
 const resultTableBody = document.getElementById("result-table-body");
+const resultOveray = document.getElementById("overlay");
+const overlay = document.getElementById("overlay");
+const resultModal = document.getElementById("result-modal");
+const closeBtn = document.getElementById("overlay-close-btn");
 
-// 문제 데이터
-const input = [
-  [10, [9, 19, 20, 50, 80, 50, 20, 140]],
-  [10, [1, 1, 1, 18, 1, 20, 1, 1, 1, 100]],
-  [5, [10, 3, 2, 8, 1, 6, 1, 4, 2, 5]],
-];
-const expectiveValue = [398, -1, -1];
+overlay.addEventListener("click", (e) => {
+  e.currentTarget.classList.add("hidden");
+});
+
+resultModal.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+closeBtn.addEventListener("click", (e) => {
+  overlay.classList.add("hidden");
+});
 
 //DOM Load되었을 때 실행
-document.addEventListener("DOMContentLoaded", () => {
+window.onload = () => {
   let lineNumberText = "";
   for (let i = 1; i < 500; i++) {
     lineNumberText += i + "\n";
   }
   lines.value = lineNumberText;
-});
+};
 
 //제출, 실행
 submitBtn.addEventListener("click", (e) => {
   try {
-    const wrappedCode = `(str, weed) => { ${codeEditor.value} }`;
+    const wrappedCode = `(arg) => { ${codeEditor.value} }`;
     const solution = new Function("return " + wrappedCode)();
     solve(solution);
   } catch (err) {
-    outputTr.children[3].textContent = err;
+    resultTableBody.children[1].children[3].textContent = err;
   }
 });
 
@@ -76,52 +108,68 @@ codeEditor.addEventListener("scroll", () => {
 
 // 함수 실행
 function solve(callback) {
+  let output = { value: "" }
+
+  overrideLog(output);
+  solveAllProblem(callback, output);
+  returnLog();
+
   // 데이터를 가로채기 위해 console.log 원본 저장 후 override;
-  // 데이터 구조까지 가져오지는 못함.
-  let output = "";
-  let originLog = console.log;
+  // string형태로 가져오기 때문에 데이터 구조까지 가져오지는 못함.
+}
 
+let originLog = console.log;
+
+function overrideLog(output) {
   console.log = (...args) => {
-    output += args.map(String).join(" ") + "\n";
+    output.value += args.map(String).join(" ") + "\n";
   };
+}
 
-  for (let i = 0; i < input.length; i++) {
-    let outputTr = resultTableBody.children[i];
-    output = "";
-    let result = 0;
-
-    try {
-      result = callback(...input[i]);
-    } catch (err) {
-      output += "[에러] : \n" + err;
-    }
-
-    outputTr.children[0].textContent = JSON.stringify(input[i]);
-    outputTr.children[1].textContent = expectiveValue[i];
-    outputTr.children[2].textContent = result;
-    outputTr.children[3].textContent = output;
-    outputTr.classList.remove("success", "fail");
-    if (expectiveValue[i] === result) {
-      outputTr.classList.add("success");
-      outputTr.children[4].textContent = "정답!";
-    } else {
-      outputTr.classList.add("fail");
-      outputTr.children[4].textContent = "실패";
-    }
-  }
-
-  //원본으로 되돌리기
+function returnLog() {
   console.log = originLog;
 }
 
-function sol(index) {
-  let output = "";
-
-  try {
-    console.log(sols[index]());
-  } catch (err) {
-    console.log(err, "에러 발생");
+// 기능 분리
+function solveAllProblem(callback, output) {
+  for (let i = 0; i < input.length; i++) {
+    solveEachProblem(callback, output, i);
   }
 }
 
-sol(pageIndex);
+//각각 문제 풀고 결과 리턴
+function solveEachProblem(callback, output, index) {
+  let result = 0;
+  output.value = "";
+
+  // arg가 더 적절할까?
+  try {
+    result = callback(...input[index]);
+  } catch (err) {
+    output += "[에러] : \n" + err;
+  }
+
+  drawView(index, result, output.value);
+}
+
+//그리기
+function drawView(index, result, output) {
+  let outputTr = resultTableBody.children[index];
+  outputTr.children[0].textContent = JSON.stringify(input[index]);
+  outputTr.children[1].textContent = expectiveValue[index];
+  outputTr.children[2].textContent = result;
+  outputTr.children[3].textContent = output;
+
+  outputTr.classList.remove("success", "fail");
+  if (expectiveValue[index] === result) {
+    outputTr.classList.add("success");
+    outputTr.children[4].textContent = "정답!";
+    resultOveray.classList.remove("hidden");
+  } else {
+    outputTr.classList.add("fail");
+    outputTr.children[4].textContent = "실패";
+  }
+}
+
+//반드시 한번은 수행됨.
+solve(sols[pageIndex]);
--- src/work2.js
+++ src/work2.js
@@ -12,9 +12,35 @@
 풀이 : 문제 풀이 
 */
 
-export function solution(){
-  let output = 0
-  var a = 12;
-  output = a;
-  return output
+console.log(calcJunukTime(13,20));
+
+function calcJunukTime(hour, minute) {
+  /* ----------------------- 제한 조건 설정 -------------------------------- */
+  /* --------- 알고리즘 문제를 풀땐 따로 설정하지 않아도 괜찮지만 우리는 공부중이니까요^^ ---- */
+  if ( (hour % 1 !== 0) || (minute % 1 !== 0) || (hour < 0) || (minute < 0) || (hour >= 24) || (minute >= 60) ) {
+    return ('당신은 혹시 시간의 마법사인가요?');
+  } else {
+    let junukHour = hour;
+    let junukMinute = minute;
+    if ((hour >= 8) && (hour < 10)) {
+      return '10:10';
+    } else if ((hour >= 10) && (hour < 15)) {
+      junukHour = hour + 1;
+      junukMinute = minute + 20;
+      return calcTime(junukHour, junukMinute);
+    } else if ((hour >= 15) && (hour < 23)) {
+      junukMinute = minute + 45;
+      return calcTime(junukHour, junukMinute);  
+    } else {
+      return '자라 🐢';
+    }
+  }
 }
+function calcTime(hour, minute) {
+  if (minute >= 60) {
+    hour += 1;
+    minute -= 60;
+  }
+  return `${hour}:${minute}`;
+}
+
